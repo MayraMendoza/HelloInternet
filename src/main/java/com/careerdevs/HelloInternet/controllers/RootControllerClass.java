4e5f23ff61ff6eb4c08c0e417eb45806c0d98406
MayraMendoza <mayra601m@gmail.com>
2022-03-03 19:08:40 -0500

    Added Get request /random & /joke

--- src/main/java/com/careerdevs/HelloInternet/controllers/RootControllerClass.java
+++ src/main/java/com/careerdevs/HelloInternet/controllers/RootControllerClass.java
@@ -4,11 +4,38 @@ package com.careerdevs.HelloInternet.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 @RestController
 public class RootControllerClass {
 
     @GetMapping("/")
     private String rootRoute (){
         return "you requested the root route";
+    }
+
+    @GetMapping("/random")
+    private int randomNumber (){
+        int min = 1;
+        int max = 100;
+        Random randomNum = new Random();
+        int generateRandomNum = min + randomNum.nextInt(max);
+        return generateRandomNum;
+    }
+
+    @GetMapping ("/joke")
+    private String joke(){
+        String jokes[] = new String[] {"What kind of exercise do lazy people do? Diddly-squats.","What do you call a pony with a cough? A little horse!",
+                "What is Forrest Gump's password? 1Forrest1.", "Why did the M&M go to school? He wanted to be a Smartie.", "What did one traffic light say to the other? Stop looking at me, I'm changing!",
+                "I invented a new word today: Plagiarism."};
+        int max = jokes.length -1;
+        Random random = new Random();
+        int generateRandomNum = random.nextInt(max);
+
+        return jokes[generateRandomNum];
+
+
+
     }
 }
